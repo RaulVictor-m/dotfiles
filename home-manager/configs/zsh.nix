14955775358449782c988e7d4e226710e7b94ee2
RaulVictor-m <raulvictor822@gmail.com>
2025-10-16 14:38:45 -0300

    quick fix zsh

--- home-manager/configs/zsh.nix
+++ home-manager/configs/zsh.nix
@@ -14,14 +14,26 @@
     autosuggestion.enable = true;
 
     initContent = ''
+      ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(
+        zhm_history_prev
+        zhm_history_next
+        zhm_prompt_accept
+        zhm_accept
+        zhm_accept_or_insert_newline
+      )
+      ZSH_AUTOSUGGEST_ACCEPT_WIDGETS+=(
+        zhm_move_right
+        zhm_clear_selection_move_right
+      )
+      ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS+=(
+        zhm_move_next_word_start
+        zhm_move_next_word_end
+      )
 
-      git_prompt() {
-        local branch
-        branch=$(git symbolic-ref --short HEAD 2>/dev/null)
-        [[ -n $branch ]] && echo "($branch)"
-      }
       setopt prompt_subst
-      PROMPT=$'\n%F{130}%~%f %F{14}$(git branch 2>/dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/(\1)/")%f\n\n%F{128}--%F{197}>%f'
+      PROMPT=$'\n%F{130}%~%f %F{14}%f%F{128}%F{197}> %f'
+
+      # PROMPT=$'\n%F{130}%~%f %F{14}$(git branch 2>/dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/(\1)/")%f\n\n%F{128}--%F{197}>%f'
     '';
   };
 }
