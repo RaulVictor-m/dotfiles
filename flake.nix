6a370bc32f10ec62b06ce8f36ca3c8a64617befa
RaulVictor-m <raulvictor822@gmail.com>
2025-12-03 14:10:58 -0300

    cleaning flake.nix home-manager

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
@@ -15,37 +16,21 @@
   let
     system = "x86_64-linux";
     user = "raul";
+
     pkgs = import nixpkgs {
       inherit system; config.allowUnfree = true;
       overlays = [zsh-hlx.overlays.default];
     };
     pkgs-unstable = import nixpkgs-unstable { inherit system; config.allowUnfree = true; };
 
-    hosts = import ./host/hosts.nix {inherit pkgs pkgs-unstable user nixpkgs;};
+    hosts-nixos = import ./host/hosts.nix {inherit pkgs pkgs-unstable user nixpkgs;};
+    hosts-hm = import ./home-manager/hosts.nix {inherit pkgs user home-manager;};
 
   in {
-    nixosConfigurations = hosts;
 
-    homeConfigurations."${user}" = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      modules = [
-        ./home-manager/home.nix
-        ./home-manager/configs/devTools.nix
-        ./home-manager/configs/wm.nix
+    nixosConfigurations = hosts-nixos;
+    homeConfigurations = hosts-hm;
 
-        ./home-manager/configs/kak.nix
-        ./home-manager/configs/fish/fish.nix
-        ./home-manager/configs/zsh.nix
-        ./home-manager/configs/editorconfig.nix
-        ./home-manager/configs/qutebrowser.nix
-        ./home-manager/configs/alacritty.nix
-      ];
-
-      extraSpecialArgs = {
-          inherit user;
-      };
-
-    };
     packages.${system} = { stable = pkgs; unstable = pkgs-unstable;};
   };
 }
---
+++ home-manager/hosts.nix
@@ -0,0 +1,23 @@
+{pkgs, user, home-manager}:
+
+{
+  "${user}" = home-manager.lib.homeManagerConfiguration {
+    inherit pkgs;
+    modules = [
+      ./home.nix
+      ./configs/devTools.nix
+      ./configs/wm.nix
+
+      ./configs/kak.nix
+      ./configs/fish/fish.nix
+      ./configs/zsh.nix
+      ./configs/editorconfig.nix
+      ./configs/qutebrowser.nix
+      ./configs/alacritty.nix
+    ];
+
+    extraSpecialArgs = {
+        inherit user;
+    };
+  };
+}
