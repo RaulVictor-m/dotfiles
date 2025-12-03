efc908be47d67488f80493754def0fb204f86568
RaulVictor-m <raulvictor822@gmail.com>
2025-12-03 14:33:43 -0300

    grub changes

--- host/desktop/system.nix
+++ host/desktop/system.nix
@@ -4,6 +4,9 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.theme = "${pkgs.kdePackages.breeze-grub}/grub/themes/breeze";
+
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
