13059b41123b21175b969647218929ddae362f7b
RaulVictor-m <raulvictor822@gmail.com>
2025-12-03 04:00:00 -0300

    blueman

--- host/desktop/configuration.nix
+++ host/desktop/configuration.nix
@@ -5,6 +5,8 @@
 
   #bluetooth
   hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+  # services.blueman-applet.enable = true;
 
   environment.systemPackages = with pkgs; [
     bluez
