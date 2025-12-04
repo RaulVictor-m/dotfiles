44eb3d622e457015048b12e245a4167e94063fd0
RaulVictor-m <raulvictor822@gmail.com>
2025-12-04 00:51:59 -0300

    giving up on gtk

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -5,13 +5,5 @@
   home.homeDirectory = "/home/${user}";
   home.stateVersion = "25.05";
 
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome-themes-extra;
-    };
-  };
-
   programs.home-manager.enable = true;
 }
