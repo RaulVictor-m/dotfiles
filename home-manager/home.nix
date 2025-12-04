c9e726851041043a3c43476faf9b1d8edf470757
RaulVictor-m <raulvictor822@gmail.com>
2025-12-04 00:22:15 -0300

    gtk theme

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -5,5 +5,13 @@
   home.homeDirectory = "/home/${user}";
   home.stateVersion = "25.05";
 
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Materia-dark";
+      package = pkgs.materia-theme;
+    };
+  };
+
   programs.home-manager.enable = true;
 }
