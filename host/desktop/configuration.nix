7901269416d06f7a2a18c92320e02ff913b7692a
RaulVictor-m <raulvictor822@gmail.com>
2025-12-04 01:45:26 -0300

    fixing bluetooth

--- host/desktop/configuration.nix
+++ host/desktop/configuration.nix
@@ -4,7 +4,22 @@
   programs.steam.enable = true;
 
   #bluetooth
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    package = pkgs.bluez;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
+        ControllerMode = "dual";
+      };
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+  };
+
   services.blueman.enable = true;
 
   #packages
