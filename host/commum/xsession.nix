c105c048ea7d538269fbf710663272242f8b0340
RaulVictor-m <raulvictor822@gmail.com>
2025-12-04 02:18:20 -0300

    fixing gtk

--- host/commum/xsession.nix
+++ host/commum/xsession.nix
@@ -3,6 +3,8 @@
 {
   services.xserver.enable = true;
   services.xserver.displayManager.startx.enable = true;
+
+  services.xserver.windowManager.i3.enable = true;
   services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
 }
