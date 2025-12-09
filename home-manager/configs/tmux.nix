bd1a204addaa0f372f1be2291211932667570c35
RaulVictor-m <raulvictor822@gmail.com>
2025-12-09 14:41:06 -0300

    fixing tmux config error

--- home-manager/configs/tmux.nix
+++ home-manager/configs/tmux.nix
@@ -8,11 +8,12 @@
     prefix = "C-Space";
     baseIndex = 1;
     historyLimit = 5000;
-    escapeTime = 10;
-    extraConfig = ''
 
+    escapeTime = 10;
+
+    extraConfig = ''
       # set -g default-terminal "xterm-256color"
-      #
+      
       set -ag terminal-overrides ",xterm-256color:RGB"
       set-option -a terminal-features "alacritty:RGB"
       set -sg repeat-time 600
