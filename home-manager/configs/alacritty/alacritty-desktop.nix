d1d9e0ee249df50622a9fe3fef16ea8268179737
RaulVictor-m <raulvictor822@gmail.com>
2025-12-09 14:45:04 -0300

    integrating tmux on alacritty

--- home-manager/configs/alacritty/alacritty-desktop.nix
+++ home-manager/configs/alacritty/alacritty-desktop.nix
@@ -3,10 +3,10 @@
 {
   programs.alacritty.enable = true;
   programs.alacritty.settings.env.TERM = "xterm-256color";
-  # programs.alacritty.settings.font.size = 6;
-  # programs.alacritty.settings.font.normal.family = "monospace";
   programs.alacritty.settings.window.padding = {x = 15; y = 5;};
 
-  programs.alacritty.settings.window.shell.program = "${pkgs.tmux}/bin/tmux";
-  programs.alacritty.settings.window.shell.args = "new-session -A -s main";
+  programs.alacritty.settings.terminal.shell = {
+    program = "${pkgs.tmux}/bin/tmux";
+    args = ["new-session" "-A" "-s" "main"];
+  };
 }
--- home-manager/configs/alacritty/alacritty.nix
+++ home-manager/configs/alacritty/alacritty.nix
@@ -3,7 +3,10 @@
 {
   programs.alacritty.enable = true;
   programs.alacritty.settings.env.TERM = "xterm-256color";
-  # programs.alacritty.settings.font.size = 16;
-  # programs.alacritty.settings.font.normal.family = "monospace";
   programs.alacritty.settings.window.padding = {x = 20; y = 20;};
+
+  programs.alacritty.settings.terminal.shell = {
+    program = "${pkgs.tmux}/bin/tmux";
+    args = ["new-session" "-A" "-s" "main"];
+  };
 }
