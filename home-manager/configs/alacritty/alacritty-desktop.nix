e1aab93a7c8678d1627672f452e7c4827acb819a
RaulVictor-m <raulvictor822@gmail.com>
2025-12-09 14:37:03 -0300

    default to tmux session on alacrity

--- home-manager/configs/alacritty/alacritty-desktop.nix
+++ home-manager/configs/alacritty/alacritty-desktop.nix
@@ -6,4 +6,7 @@
   # programs.alacritty.settings.font.size = 6;
   # programs.alacritty.settings.font.normal.family = "monospace";
   programs.alacritty.settings.window.padding = {x = 15; y = 5;};
+
+  programs.alacritty.settings.window.shell.program = "${pkgs.tmux}/bin/tmux";
+  programs.alacritty.settings.window.shell.args = "new-session -A -s main";
 }
