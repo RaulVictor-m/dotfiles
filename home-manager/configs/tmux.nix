0aebdbbe24058dc45745ccb7e7c3782605cecd2d
RaulVictor-m <raulvictor822@gmail.com>
2025-12-10 10:05:45 -0300

    fixing broken tmux color

--- home-manager/configs/tmux.nix
+++ home-manager/configs/tmux.nix
@@ -4,7 +4,6 @@
   programs.tmux = {
     enable = true;
     keyMode = "vi";
-    terminal = "screen-256color";
     prefix = "C-Space";
     baseIndex = 1;
     historyLimit = 5000;
@@ -12,8 +11,7 @@
     escapeTime = 10;
 
     extraConfig = ''
-      # set -g default-terminal "xterm-256color"
-      
+      set -g default-terminal "xterm-256color"
       set -ag terminal-overrides ",xterm-256color:RGB"
       set-option -a terminal-features "alacritty:RGB"
       set -sg repeat-time 600
