1d8c98fd8162fe730076a0ea6a38d5968d069acb
RaulVictor-m <raulvictor822@gmail.com>
2025-12-30 23:17:49 -0300

    grub menu and osprober

--- host/desktop/system.nix
+++ host/desktop/system.nix
@@ -1,14 +1,25 @@
 { config, lib, pkgs, ... }:
 
 {
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.useOSProber = true;
   boot.loader.grub.theme = "${pkgs.kdePackages.breeze-grub}/grub/themes/breeze";
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.extraEntries = ''
+    menuentry "Reboot" {
+      reboot
+    }
+    menuentry "Poweroff" {
+      halt
+    }
+    menuentry "uefi-firmware" {
+      fwsetup
+    }
+  '';
 
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   swapDevices = [
     {
