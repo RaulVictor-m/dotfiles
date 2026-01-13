07f6e8c9c15b7548482c4f927fe17d9a6999478b
RaulVictor-m <raulvictor822@gmail.com>
2026-01-13 19:26:50 -0300

    window scale on x

--- host/commum/configuration.nix
+++ host/commum/configuration.nix
@@ -4,6 +4,7 @@
   nix.settings.experimental-features = ["nix-command" "flakes"];
   environment.variables.EDITOR = "kak";
   environment.variables.TERM = "xterm-256color";
+  environment.variables.WINIT_X11_SCALE_FACTOR= 1;
   services.dbus.enable = true;
 
   system.stateVersion = "25.05";
