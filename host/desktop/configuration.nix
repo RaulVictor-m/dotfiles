ac5f005974580b852bdd2be029db894d4ad62cea
RaulVictor-m <raulvictor822@gmail.com>
2026-01-28 11:01:05 -0300

    adding openrgb

--- host/desktop/configuration.nix
+++ host/desktop/configuration.nix
@@ -2,4 +2,7 @@
 
 {
   networking.interfaces.enp5s0.wakeOnLan.enable = true;
+  services.hardware.openrgb.enable = true;
+  services.hardware.openrgb.package = pkgs.openrgb-with-all-plugins;
+  boot.kernelModules = [ "i2c-dev" ];
 }
