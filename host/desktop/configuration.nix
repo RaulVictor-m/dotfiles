111e3d020b631557b8f90da9944f59aa91dd9ced
RaulVictor-m <raulvictor822@gmail.com>
2026-01-28 11:38:23 -0300

    reverting changes

--- host/desktop/configuration.nix
+++ host/desktop/configuration.nix
@@ -2,7 +2,4 @@
 
 {
   networking.interfaces.enp5s0.wakeOnLan.enable = true;
-  services.hardware.openrgb.enable = true;
-  services.hardware.openrgb.package = pkgs.openrgb-with-all-plugins;
-  boot.kernelModules = [ "i2c-dev" ];
 }
