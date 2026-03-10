4542657dc519a07e0812e36e0316737cf8c03d0f
RaulVictor-m <raulvictor822@gmail.com>
2026-03-10 01:11:39 -0300

    man pages

--- host/commum/configuration.nix
+++ host/commum/configuration.nix
@@ -6,6 +6,7 @@
   environment.variables.TERM = "xterm-256color";
   environment.variables.WINIT_X11_SCALE_FACTOR= 1;
   services.dbus.enable = true;
+  documentation.dev.enable = true;
 
   system.stateVersion = "25.05";
 }
--- host/commum/packages.nix
+++ host/commum/packages.nix
@@ -69,6 +69,8 @@
     xclip
     xdo
     xdotool
+    man-pages
+    man-pages-posix
 
     # utils
     brightnessctl
