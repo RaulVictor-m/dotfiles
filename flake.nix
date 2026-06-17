67f20560788239ae322610bed109830e501df3d6
RaulVictor-m <raulvictor822@gmail.com>
2026-06-17 08:26:19 -0300

    flake update

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "Home config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    stylix.url = "github:nix-community/stylix/release-25.11";
+    stylix.url = "github:nix-community/stylix/release-26.05";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
 
     zsh-hlx.url = "github:multirious/zsh-helix-mode/main";
